00d8686cc89688cbe873e5fc4ac9a1f5a466c859
kiri1101 <icejordan0@gmail.com>
2025-03-30 00:25:22 +0100

    optimize ecosystem.config file to ES6 format

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  apps: [
-    {
-      name: 'nuxt-app',
-      script: './start.js',
-      env: {
-        HOST: 'localhost',
-        PORT: 3000
-      }
+export const apps = [
+  {
+    name: 'nuxt-app',
+    script: './start.js',
+    env: {
+      HOST: 'localhost',
+      PORT: 3000
     }
-  ]
-}
+  }
+];
