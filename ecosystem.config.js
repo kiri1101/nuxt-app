dd7a8a5655b3afcb01acc8dc7179bca835ce5590
kiri1101 <icejordan0@gmail.com>
2025-03-30 01:34:07 +0100

    update ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,16 @@ export const apps = [
   {
     name: 'nuxt-app',
     exec_mode: 'cluster',
-    instances: 'max',
-    script: './node_modules/nuxt/bin/nuxt.js',
-    args: 'start',
+    instances: '1', // Start with 1 instance first
+    script: 'npm',
+    args: 'run start',
+    cwd: '/home/forge/devfont.compbook.online',
     env: {
       NITRO_PORT: 3000,
-      NODE_ENV: 'production'
-    }
+      NODE_ENV: 'production',
+      HOST: '0.0.0.0' // Important for external access
+    },
+    error_file: '/home/forge/.pm2/logs/nuxt-app-error.log',
+    out_file: '/home/forge/.pm2/logs/nuxt-app-out.log'
   }
 ];
