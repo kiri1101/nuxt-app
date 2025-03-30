7ad1622b561e85ea87dac8df5625364c455c084d
kiri1101 <icejordan0@gmail.com>
2025-03-30 01:46:29 +0100

    add start script to package.json & optimized ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,14 @@ export const apps = [
   {
     name: 'nuxt-app',
     exec_mode: 'cluster',
-    instances: '1', // Start with 1 instance first
+    instances: '1',
     script: 'npm',
-    args: 'run start',
+    args: 'run preview',  // Changed from 'start' to 'preview'
     cwd: '/home/forge/devfont.compbook.online',
     env: {
       NITRO_PORT: 3000,
       NODE_ENV: 'production',
-      HOST: '0.0.0.0' // Important for external access
-    },
-    error_file: '/home/forge/.pm2/logs/nuxt-app-error.log',
-    out_file: '/home/forge/.pm2/logs/nuxt-app-out.log'
+      HOST: '0.0.0.0'
+    }
   }
 ];
